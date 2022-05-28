33398f097e602000b822fca6a2c92a4ac13198c4
Luong Le <luong@originalstitch.com>
2022-05-28 09:05:32 +0700

    Add Mixpanel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 import 'bootstrap';
-import 'chart.js';
 
+import 'chart.js';
 import Chart from 'chart.js/auto';
 window.Chart = Chart;
+
+import mixpanel from 'mixpanel-browser';
+window.mixpanel = mixpanel;
+mixpanel.init('7ca7759b2c50126ea905f5b4598a14c2', {debug: false});
 
