312824542fe5f6f71481c813be7d235df79c92ca
Luong Le <luong@originalstitch.com>
2022-06-12 02:10:14 +0700

    Improve admin transactions page

--- resources/views/elements/donate-model.blade.php
+++ resources/views/elements/donate-model.blade.php
@@ -5,7 +5,7 @@
                 <h5 class="modal-title" id="staticBackdropLabel">My Theta Wallet Address</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
+            <div class="modal-bod text-break p-2">
                 0x77B4a8bcEaC10762b4E5902e29428DbfB4348003
             </div>
         </div>
--- resources/views/elements/top-transactions-card.blade.php
+++ resources/views/elements/top-transactions-card.blade.php
@@ -8,11 +8,11 @@
             @foreach ($topTransactions as $hash => $transaction)
                 <div class="row">
                     @if ($transaction['type'] == 'transfer')
-                        <span class="bullet h-auto">T</span><a href="{{ Helper::makeThetaTransactionURL($hash) }}" target="_blank" class="w-auto p-0">{{ $transaction['amount'] }}</a> <span class="d-none d-lg-inline w-auto p-0 ps-1">transferred on {{ $transaction['date'] }}</span>
+                        <span class="bullet h-auto">T</span><a href="/transaction/{{ $hash }}" class="w-auto p-0">{{ $transaction['amount'] }}</a> <span class="d-none d-lg-inline w-auto p-0 ps-1">transferred on {{ $transaction['date'] }}</span>
                     @elseif ($transaction['type'] == 'stake')
-                        <span class="bullet h-auto">S</span><a href="{{ Helper::makeThetaTransactionURL($hash) }}" target="_blank" class="w-auto p-0">{{ $transaction['amount'] }}</a> <span class="d-none d-lg-inline w-auto p-0 ps-1">staked on {{ $transaction['date'] }}</span>
+                        <span class="bullet h-auto">S</span><a href="/account/{{ explode('_', $hash)[2] }}" class="w-auto p-0">{{ $transaction['amount'] }}</a> <span class="d-none d-lg-inline w-auto p-0 ps-1">staked on {{ $transaction['date'] }}</span>
                     @elseif ($transaction['type'] == 'withdraw')
-                        <span class="bullet h-auto">W</span><a href="{{ Helper::makeThetaAccountURL($transaction['from']) }}" target="_blank" class="w-auto p-0">{{ $transaction['amount'] }}</a> <span class="d-none d-lg-inline w-auto p-0 ps-1">withdrawn on {{ $transaction['date'] }}</span>
+                        <span class="bullet h-auto">W</span><a href="/account/{{ explode('_', $hash)[2] }}" class="w-auto p-0">{{ $transaction['amount'] }}</a> <span class="d-none d-lg-inline w-auto p-0 ps-1">withdrawn on {{ $transaction['date'] }}</span>
                     @endif
                 </div>
             @endforeach
