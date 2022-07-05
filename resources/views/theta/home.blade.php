089ee36c01f4a46cec6041d3a79e68dbb3ff042e
Luong Le <luong@originalstitch.com>
2022-07-05 13:21:47 +0700

    Add GTM

--- resources/views/theta/home.blade.php
+++ resources/views/theta/home.blade.php
@@ -7,7 +7,6 @@
         @include('elements/tdrop-card', ['coinInfo' => $coins['TDROP']])
         @include('elements/theta-stake-chart-card')
         @include('elements/tfuel-stake-chart-card')
-        @include('elements/elite-node-chart-card')
         @include('elements/top-transactions-card')
         @include('elements/unstakings-card')
         @include('elements/validators-card')
