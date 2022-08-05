3d62393e978c1af8940dbd62d750f7ffe3b2288d
Luong Le <luong@originalstitch.com>
2022-08-05 22:16:38 +0700

    Web3

--- app/Mail/WalletRadarEmail.php
+++ app/Mail/WalletRadarEmail.php
@@ -10,14 +10,16 @@ class WalletRadarEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    private $params;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($params)
     {
-        //
+        $this->params = $params;
     }
 
     /**
@@ -27,6 +29,10 @@ class WalletRadarEmail extends Mailable
      */
     public function build()
     {
-        return $this->view('emails.wallet_radar');
+        $subject = 'Activities of Your Theta Wallet';
+        $message = 'We just detected a ' . $this->params['activity'] . ' from your theta wallet. Please <a href="https://explorer.thetatoken.org/account/' . $this->params['account'] . '">check it here</a>';
+        return $this->subject($subject)->view('emails.wallet_radar')->with([
+            'notif' => $message
+        ]);
     }
 }
--- resources/views/emails/wallet_radar.blade.php
+++ resources/views/emails/wallet_radar.blade.php
@@ -1,1 +1,2 @@
-1234
+<p>{!! $notif !!}</p>
+<p>To unsubscribe from receiving this notification, please <a href="https://thetapizza.com/subscribe">use Untrack function</a>.</p>
