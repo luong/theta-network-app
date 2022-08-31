078902eb9e5b4e01fa0ebf11baa6e23fae2d5ee1
Luong Le <luong@originalstitch.com>
2022-08-31 19:23:20 +0700

    Refactor

--- resources/views/elements/top-transactions-card.blade.php
+++ resources/views/elements/top-transactions-card.blade.php
@@ -6,9 +6,20 @@
     <div class="card-body scrollable">
         <div class="container">
             @foreach ($topTransactions as $transaction)
+                @php
+                    $extra = '';
+                    $whale = '';
+                    if (isset($accounts[$transaction['from_account']])) {
+                        $extra = 'whale';
+                        $whale = $accounts[$transaction['from_account']]['name'];
+                    } else if (isset($accounts[$transaction['to_account']])) {
+                        $extra = 'whale';
+                        $whale = $accounts[$transaction['to_account']]['name'];
+                    }
+                @endphp
                 <div class="row">
                     @if ($transaction['type'] == 'transfer')
-                        <span class="bullet h-auto">Transfer</span> <x-currency type="{{ $transaction['currency'] }}"/> <a href="{{ Helper::makeSiteTransactionURL($transaction['txn'], $transaction['currency']) }}" class="w-auto ps-1 pe-1">{{ number_format($transaction['coins'], 0) }} ({{ Helper::formatPrice($transaction['usd']) }})</a>
+                        <span class="bullet h-auto {{ $extra }}" title="{{ $whale }}">Transfer</span> <x-currency type="{{ $transaction['currency'] }}"/> <a href="{{ Helper::makeSiteTransactionURL($transaction['txn'], $transaction['currency']) }}" class="w-auto ps-1 pe-1">{{ number_format($transaction['coins'], 0) }} ({{ Helper::formatPrice($transaction['usd']) }})</a>
                     @elseif ($transaction['type'] == 'stake')
                         <span class="bullet h-auto">Stake</span> <x-currency type="{{ $transaction['currency'] }}"/> <a href="{{ Helper::makeSiteTransactionURL($transaction['txn'], $transaction['currency']) }}" class="w-auto ps-1 pe-1">{{ number_format($transaction['coins'], 0) }} ({{ Helper::formatPrice($transaction['usd']) }})</a>
                     @elseif ($transaction['type'] == 'unstake')
