a0704bd4b42814e8a3c22ff016218fe855b19271
Luong Le <luong@originalstitch.com>
2022-09-06 14:26:29 +0700

    Add transactions page

--- resources/views/theta/transactions.blade.php
+++ resources/views/theta/transactions.blade.php
@@ -93,7 +93,7 @@
                 @endforeach
                 </tbody>
             </table>
-            {{ $transactions->links() }} (Transactions collected in 7 days)
+            {{ $transactions->onEachSide(2)->links() }} (Transactions collected in 7 days)
         </div>
 
     </div>
